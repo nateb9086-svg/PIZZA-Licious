54e231f51eaa256c044a9ac1cefe41a3d8cb4fb4
natefrezzell <nateb9086@gmail.com>
2026-05-22 13:41:39 -0400

    Added new classes

--- src/main/java/com/pluralsight/PizzaScreen.java
+++ src/main/java/com/pluralsight/PizzaScreen.java
@@ -4,15 +4,16 @@ import java.util.List;
 
 public class PizzaScreen {
 
-    public static buildPizza(){
+    public static buildPizza() {
 
         System.out.println("");
         System.out.println("It's time to build your Pizza");
         System.out.println("");
 
 
-        List<String> types = List.of("Thin Crust", "Regular Crust",
-                "Thick Crust", "cauliflower");
+        List<String> types = List.of("Thin Crust", "Regular Crust", "Thick Crust", "cauliflower");
+
+        List<String> sizes = List.of("Small (8\") - $ 8.50", "Medium (12\") - $12.00", "Large (16\") - $16.50");
 
 
     }
